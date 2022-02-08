bb09b909241735beabaf56844c7fa3bf8633faeb
Amarachi Akunna <akunna1@live.unc.edu>
2022-02-07 21:28:29 -0500

    Random commit

---
+++ Assignment_2.R
@@ -0,0 +1,9 @@
+# Checking my working directory
+getwd()# returns an absolute filepath representing the current working directory
+
+# Importing my .csv data to the Global Environment
+fire_incidents_data <- read.csv("Fire_Incidents.csv", header = TRUE, sep = ",")
+#code to import a dataset and name it fire_incidents_data
+fire_incidents_data
+
+#Questions:
